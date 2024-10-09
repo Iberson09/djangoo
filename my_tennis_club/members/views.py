e0ad40d5e81fc2d484a7a3ded178629a7d271c82
Jhon Iberson Mariñas <96397779+Iberson09@users.noreply.github.com>
2024-10-09 16:37:53 +0000

    views.py

--- my_tennis_club/members/views.py
+++ my_tennis_club/members/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import renderfrom django.http import HttpResponse
+from django.shortcuts import render
+from django.http import HttpResponse
 
-def members(request):return HttpResponse("Hello world!")
+def members(request):
+    return HttpResponse("Hello world!")
